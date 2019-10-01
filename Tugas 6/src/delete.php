82f93bc937da1f9892ebc07e9640527534163377
mramirid <amir.rhythm@gmail.com>
2019-10-01 17:19:56 +0700

    Added delete action

---
+++ Tugas 6/src/delete.php
@@ -0,0 +1,14 @@
+<?php 
+    include('conn.php');
+
+    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+        if (isset($_GET['no'])) {
+            $noDelete = $_GET['no'];
+            $deleteQuery = "DELETE FROM `profile` WHERE `no` = '$noDelete'"; 
+            mysqli_query(connection(), $deleteQuery);
+  
+            // Kembali ke home
+            header('Location: index.php?status='.$status);
+        }  
+    }
+?>
