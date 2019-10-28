52d672cb6c66914063aba8e61a8f211f810086e3
mramirid <amir.rhythm@gmail.com>
2019-10-28 15:23:43 +0700

    Added class Predikat

---
+++ Tugas 7/Predikat.php
@@ -0,0 +1,18 @@
+<?php 
+
+class Predikat
+{
+    public const A = "A";
+    public const B = "B";
+    public const C = "C";
+    public const D = "D";
+    public const E = "E";
+
+    public const MULTIPLIER_A = 4;
+    public const MULTIPLIER_B = 3;
+    public const MULTIPLIER_C = 2;
+    public const MULTIPLIER_D = 1;
+    public const MULTIPLIER_E = 0;
+}
+
+?>
