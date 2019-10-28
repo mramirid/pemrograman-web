f4806ecb2cfa2b6af643c96e23c23995f176cdf2
mramirid <amir.rhythm@gmail.com>
2019-10-28 15:23:12 +0700

    Added class Mahasiswa

---
+++ Tugas 7/Mahasiswa.php
@@ -0,0 +1,73 @@
+<?php 
+
+class Mahasiswa
+{
+    private $npm = NULL;
+    private $nama = NULL;
+    private $prodi = NULL;
+    private $fakultas = NULL;
+    private $listMataKuliah = NULL;
+    private $listNilai = NULL;
+
+    public function setNPM($npm)
+    {
+        $this->npm = $npm;
+    }
+
+    public function getNPM()
+    {
+        return $this->npm;
+    }
+
+    public function setNama($nama)
+    {
+        $this->nama = $nama;
+    }
+
+    public function getNama()
+    {
+        return $this->nama;
+    }
+
+    public function setProdi($prodi)
+    {
+        $this->prodi = $prodi;
+    }
+
+    public function getProdi()
+    {
+        return $this->prodi;
+    }
+
+    public function setFakultas($fakultas)
+    {
+        $this->fakultas = $fakultas;
+    }
+
+    public function getFakultas()
+    {
+        return $this->fakultas;
+    }
+
+    public function setListMataKuliah($listMataKuliah)
+    {
+        $this->listMataKuliah = $listMataKuliah;
+    }
+
+    public function getListMataKuliah()
+    {
+        return $this->listMataKuliah;
+    }
+
+    public function setListNilai($listNilai)
+    {
+        $this->listNilai = $listNilai;
+    }
+
+    public function getListNilai()
+    {
+        return $this->listNilai;
+    }
+}
+
+?>
